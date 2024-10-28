3777b7d89b7f67f252ea7167316a7d33d47a320e
Budnikowski Slawomir <slawomir.budnikowski@inetum.com>
2024-10-28 10:45:52 +0100

    tracing changes in .net app

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,57 +1,85 @@
 using BookStoreApi.Models;
 using BookStoreApi.Services;
 using Examples.AspNetCore;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Instrumentation.AspNetCore;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using System.Reflection.PortableExecutable;
 
-var builder = WebApplication.CreateBuilder(args);
+var appBuilder = WebApplication.CreateBuilder(args);
 
 const string serviceName = "bookstore-api";
 
 var otlpHostName = Environment.GetEnvironmentVariable("OTLP_HOSTNAME") ?? "localhost";
 
-builder.Logging.ClearProviders()
-    .AddOpenTelemetry(options =>
+appBuilder.Services.AddSingleton<Instrumentation>();
+
+appBuilder.Logging.ClearProviders();
+
+appBuilder.Services.AddOpenTelemetry()
+    .ConfigureResource(r => r
+        .AddService(
+            serviceName: serviceName,
+            serviceVersion: typeof(Program).Assembly.GetName().Version?.ToString() ?? "unknown",
+            serviceInstanceId: Environment.MachineName))
+    .WithTracing(builder =>
     {
-        options.IncludeFormattedMessage = true;
-        options.IncludeScopes = true;
-        options.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName))
-        .AddOtlpExporter(options => options.Endpoint = new Uri($"http://{otlpHostName}:4317"))
-        .AddConsoleExporter();
+        builder
+            .AddSource(Instrumentation.ActivitySourceName)
+            .SetSampler(new AlwaysOnSampler())
+            .AddHttpClientInstrumentation()
+            .AddAspNetCoreInstrumentation();
+
+        appBuilder.Services.Configure<AspNetCoreTraceInstrumentationOptions>(appBuilder.Configuration.GetSection("AspNetCoreInstrumentation"));
+
+        builder.AddOtlpExporter(otlpOptions =>
+        {
+            otlpOptions.Endpoint = new Uri($"http://{otlpHostName}:4317");
+        }).AddConsoleExporter();
+    })
+    .WithMetrics(builder =>
+    {
+        builder
+            .AddMeter(Instrumentation.MeterName)
+            .SetExemplarFilter(ExemplarFilterType.TraceBased)
+            .AddRuntimeInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddAspNetCoreInstrumentation();
+
+        builder.AddOtlpExporter(otlpOptions =>
+        {
+            otlpOptions.Endpoint = new Uri($"http://{otlpHostName}:4317");
+        }).AddConsoleExporter()
+        .AddPrometheusExporter();
+    }).WithLogging(builder =>
+    {
+        builder.AddOtlpExporter(otlpOptions =>
+        {
+            otlpOptions.Endpoint = new Uri($"http://{otlpHostName}:4317");
+        }).AddConsoleExporter();
     });
 
-builder.Services.AddOpenTelemetry()
-      .ConfigureResource(resource => resource.AddService(serviceName))
-      .WithTracing(tracing => tracing
-          .AddAspNetCoreInstrumentation()
-          .AddOtlpExporter(options => options.Endpoint = new Uri($"http://{otlpHostName}:4317"))
-          .AddConsoleExporter())
-      .WithMetrics(metrics => metrics
-          .AddAspNetCoreInstrumentation()
-          .AddOtlpExporter(options => options.Endpoint = new Uri($"http://{otlpHostName}:4317"))
-          .AddConsoleExporter());
+appBuilder.Services.Configure<BookStoreDatabaseSettings>(
+  appBuilder.Configuration.GetSection("BookStoreDatabase"));
 
-builder.Services.AddSingleton<Instrumentation>();
+appBuilder.Services.AddSingleton<BooksService>();
 
-builder.Services.Configure<BookStoreDatabaseSettings>(
-  builder.Configuration.GetSection("BookStoreDatabase"));
-
-builder.Services.AddSingleton<BooksService>();
-
-builder.Services.AddControllers()
+appBuilder.Services.AddControllers()
   .AddJsonOptions(
     options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+appBuilder.Services.AddEndpointsApiExplorer();
+appBuilder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+var app = appBuilder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 app.UseAuthorization();
 app.MapControllers();
